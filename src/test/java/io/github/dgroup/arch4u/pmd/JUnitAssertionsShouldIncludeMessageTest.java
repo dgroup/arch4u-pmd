4504dc32e3a53dd7ba48445eaea39a845fd31596
Yurii Dubinka <yurii.dubinka@gmail.com>
2024-10-14 22:37:48 +0300

    Increase code quality thresholds

--- src/test/java/io/github/dgroup/arch4u/pmd/JUnitAssertionsShouldIncludeMessageTest.java
+++ src/test/java/io/github/dgroup/arch4u/pmd/JUnitAssertionsShouldIncludeMessageTest.java
@@ -30,15 +30,12 @@ import net.sourceforge.pmd.test.SimpleAggregatorTst;
  * Test case for {@code JUnitAssertionsShouldIncludeMessage} rule.
  *
  * @since 0.1.0
- * @checkstyle MethodBodyCommentsCheck (100 lines)
- * @checkstyle CommentsIndentationCheck (100 lines)
  */
 @SuppressWarnings({"PMD.TestClassWithoutTestCases", "PMD.JUnit4TestShouldUseBeforeAnnotation"})
 public final class JUnitAssertionsShouldIncludeMessageTest extends SimpleAggregatorTst {
 
     @Override
     public void setUp() {
-        // @todo #/DEV Implementation is needed
         addRule(
             "io/github/dgroup/arch4u/pmd/arch4u-ruleset.xml",
             "JUnitAssertionsShouldIncludeMessage"
